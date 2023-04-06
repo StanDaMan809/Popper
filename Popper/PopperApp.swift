7e4b34b15b54bbe340abf26849d72e63f9e6345f
Stanley <Grullon>
2023-04-06 18:19:30 -0400

    Initial Commit

---
+++ Popper/PopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopperApp.swift
+//  Popper
+//
+//  Created by Stanley Grullon on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

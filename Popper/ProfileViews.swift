213d4f026e3a93f2c616d22c1ca7001473d565c9
Stanley <Grullon>
2023-12-01 04:29:22 -0500

    11 / 31 / 23 Update
    Added a functioning feed with proper text sizing. Moved the app down to target iOS 16.0; moved all photos to Offset and added a new layout to properly display photos. Added thumbnail rendering for profiles in preparation for custom profiles. Added comment and message struct.

---
+++ Popper/ProfileViews.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViews.swift
+//  Popper
+//
+//  Created by Stanley Grullon on 11/30/23.
+//
+
+import SwiftUI
+
+struct ProfileViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileViews()
+}
---
+++ Popper/Views/MainView/PostView/PostLayout.swift
@@ -0,0 +1,18 @@
+//
+//  PostLayout.swift
+//  Popper
+//
+//  Created by Stanley Grullon on 11/27/23.
+//
+
+import SwiftUI
+
+struct PostLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostLayout()
+}
